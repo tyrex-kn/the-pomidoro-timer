4cbb77695391d490f0668d1d31df9d1b1624f97a
Ty-rex <rexty654@gmail.com>
2026-03-26 16:39:30 +0300

    feat: add all default script

---
+++ main.js
@@ -0,0 +1,36 @@
+import {app, BrowserWindow} from 'electron/main'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    title: "",
+    width: 200,
+    height: 230,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: true,
+    background: "#00000000",
+    webPreferences: {
+        contextIsolation: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
